e17b9c41692a64912ee6182cee374bcf9c5c55e2
artur wasek <vaseque@yahoo.co.uk>
2019-12-07 12:49:28 +0100

    Add accordion function

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,151 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
-
+/* eslint-disable */
 {
-  'use strict';
+    'use strict';
 
-  const select = {
-    templateOf: {
-      menuProduct: '#template-menu-product',
-    },
-    containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
-    },
-    all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
-    },
-    menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
-    },
-    widgets: {
-      amount: {
-        input: 'input[name="amount"]',
-        linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
-  };
+    const select = {
+        templateOf: {
+            menuProduct: '#template-menu-product',
+        },
+        containerOf: {
+            menu: '#product-list',
+            cart: '#cart',
+        },
+        all: {
+            menuProducts: '#product-list > .product',
+            menuProductsActive: '#product-list > .product.active',
+            formInputs: 'input, select',
+        },
+        menuProduct: {
+            clickable: '.product__header',
+            form: '.product__order',
+            priceElem: '.product__total-price .price',
+            imageWrapper: '.product__images',
+            amountWidget: '.widget-amount',
+            cartButton: '[href="#add-to-cart"]',
+        },
+        widgets: {
+            amount: {
+                input: 'input[name="amount"]',
+                linkDecrease: 'a[href="#less"]',
+                linkIncrease: 'a[href="#more"]',
+            },
+        },
+    };
 
-  const classNames = {
-    menuProduct: {
-      wrapperActive: 'active',
-      imageVisible: 'active',
-    },
-  };
+    const classNames = {
+        menuProduct: {
+            wrapperActive: 'active',
+            imageVisible: 'active',
+        },
+    };
 
-  const settings = {
-    amountWidget: {
-      defaultValue: 1,
-      defaultMin: 1,
-      defaultMax: 9,
+    const settings = {
+        amountWidget: {
+            defaultValue: 1,
+            defaultMin: 1,
+            defaultMax: 9,
+        }
+    };
+
+    const templates = {
+        menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    };
+
+    class Product {
+        constructor(id, data) {
+
+            const thisProduct = this;
+            thisProduct.id = id;
+            thisProduct.data = data;
+            thisProduct.renderInMenu();
+            thisProduct.initAccordion();
+
+        }
+
+        renderInMenu() {
+
+            const thisProduct = this;
+
+            const generatedHTML = templates.menuProduct(thisProduct.data);
+            thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+            const menuContainer = document.querySelector(select.containerOf.menu);
+            menuContainer.appendChild(thisProduct.element);
+
+        }
+
+        initAccordion() {
+            const thisProduct = this;
+
+            /* find the clickable trigger (the element that should react to clicking) */
+
+            const clickHeader = document.querySelector(select.menuProduct.clickable);
+
+            /* START: click event listener to trigger */
+
+            thisProduct.element.addEventListener('click', function(event) {
+
+                /* prevent default action for event */
+
+                event.preventDefault();
+
+                /* toggle active class on element of thisProduct */
+
+                thisProduct.element.classList.toggle('active');
+
+                /* find all active products */
+
+                const activeProducts = document.querySelectorAll('.product.active');
+
+                /* START LOOP: for each active product */
+
+                for (let activeProduct of activeProducts) {
+
+                    /* START: if the active product isn't the element of thisProduct */
+
+                    if (activeProduct != thisProduct.element) {
+
+                        /* remove class active for the active product */
+
+                        activeProduct.classList.remove('active');
+
+                        /* END: if the active product isn't the element of thisProduct */
+                    }
+                    /* END LOOP: for each active product */
+                }
+                /* END: click event listener to trigger */
+            });
+
+        }
+
     }
-  };
 
-  const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  };
+    const app = {
+        initMenu: function() {
+            const thisApp = this;
+            console.log('thisApp.data:', thisApp.data);
+            for (let productData in thisApp.data.products) {
+                new Product(productData, thisApp.data.products[productData]);
+            }
+        },
 
-  const app = {
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-    },
-  };
+        initData: function() {
+            const thisApp = this;
+            thisApp.data = dataSource;
+        },
 
-  app.init();
+        init: function() {
+            const thisApp = this;
+            console.log('*** App starting ***');
+            console.log('thisApp:', thisApp);
+            console.log('classNames:', classNames);
+            console.log('settings:', settings);
+            console.log('templates:', templates);
+            thisApp.initData();
+            thisApp.initMenu();
+        },
+    };
+
+    app.init();
+
 }
